542b1a471936a3a9b18bd6ca20d4dc0ac82ceccc
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-03-01 11:33:49 -0500

    Create Katamari.js

---
+++ WebsiteEdit/Katamari.js
@@ -0,0 +1,1 @@
+javascript:var%20i,s,ss=['http://kathack.com/js/kh.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
