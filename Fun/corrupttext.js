e1456c6adb96de51db5775d01c77938b91c5c0dc
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-04-21 12:48:35 -0400

    Create corrupttext.js
    Changes The Text To 3D and Weird Colors

---
+++ Fun/corrupttext.js
@@ -0,0 +1,1 @@
+javascript:(function(){var s = document.createElement('style');s.innerHTML = 'body {background-color: black; color: green; font-family: Courier New; font-size: 200%; text-shadow: 2px 2px #FF0000;} * {background-color: black !important; color: green !important; font-family: Courier New !important; text-shadow: 2px 2px #FF0000 !important;}';document.body.appendChild(s);})();
