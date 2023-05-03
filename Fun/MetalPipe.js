8fcdace7aa549998bce157dc8c3a5493d9c8beab
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-05-03 17:18:16 -0400

    Create MetalPipe.js

---
+++ Fun/MetalPipe.js
@@ -0,0 +1,1 @@
+javascript:(function(){var audio=new Audio('https://www.myinstants.com/media/sounds/jixaw-metal-pipe-falling-sound.mp3');audio.play();var imgs=document.getElementsByTagName('img');for(var i=0;i<imgs.length;i++){imgs[i].src='https://i.kym-cdn.com/entries/icons/original/000/043/027/metalpipefalling.jpg';}})();
