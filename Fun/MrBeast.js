6b7bd7e33bd6004a1986d0ba34a9f76983df8637
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-05-04 08:43:59 -0400

    Create MrBeast.js

---
+++ Fun/MrBeast.js
@@ -0,0 +1,1 @@
+javascript:(function(){var audio=new Audio('https://www.myinstants.com/media/sounds/mrbeast-rap-battle-announcer.mp3');audio.play();var imgs=document.getElementsByTagName('img');for(var i=0;i<imgs.length;i++){imgs[i].src='https://media.tenor.com/IIXy6CqR5l8AAAAC/mrbeast-ytpmv.gif';}})();
