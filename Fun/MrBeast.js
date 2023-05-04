d6945871c0e65ef87a974b5bd239a4ff3f313979
MrGamz <123112118+MrGamzWasTaken@users.noreply.github.com>
2023-05-04 10:15:38 -0400

    Update MrBeast.js

--- Fun/MrBeast.js
+++ Fun/MrBeast.js
@@ -1,1 +1,9 @@
-javascript:(function(){var audio=new Audio('https://www.myinstants.com/media/sounds/mrbeast-rap-battle-announcer.mp3');audio.play();var imgs=document.getElementsByTagName('img');for(var i=0;i<imgs.length;i++){imgs[i].src='https://media.tenor.com/IIXy6CqR5l8AAAAC/mrbeast-ytpmv.gif';}})();
+javascript:(function(){
+var audio = new Audio('https://www.myinstants.com/media/sounds/mrbeast-rap-battle-announcer.mp3');
+audio.loop = true;
+audio.play();
+var imgs = document.getElementsByTagName('img');
+for(var i = 0; i < imgs.length; i++){
+imgs[i].src = 'https://media.tenor.com/IIXy6CqR5l8AAAAC/mrbeast-ytpmv.gif';
+}
+})();
